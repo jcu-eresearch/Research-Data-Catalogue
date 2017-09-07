7bdbf1b36e26cbf6ed5c11801eb95ae33fd0a3ec
Jay van Schyndel <ozej8y@gmail.com>
2017-09-07 11:01:28 +1000

    Ensure all RIF-CS is generated using Groovy, not Velocity
    In ReDBox 1.9, Groovy is used to build RIF-CS 1.6, this commit contains
    fixes preventing RIF-CS 1.3 being generated using velocity templates.

---
+++ src/main/config/home/dataFixes/upgradeTo19.sh
@@ -0,0 +1,15 @@
+# Copy the new dataset.json file into the storage folder, replacing the old versions.
+#This ensures for 1.9 that the RIF-CS is generated using Groovy and not the old RIF-VM
+cp ../harvest/workflows/dataset.json ../../storage/1793582ab247f6442162a75562dcc548/46/ae/31/46ae31c75d852149f79b61e6a219c2d9/
+cp ../harvest/workflows/dataset.json ../../storage/1793582ab247f6442162a75562dcc548/9d/f9/ed/9df9edabad280b8a1a3dc17831e475c5/
+cp ../harvest/workflows/dataset.json ../../storage/1793582ab247f6442162a75562dcc548/ac/8d/fa/ac8dfa3d130d0adf4d136ef2e372106d/
+
+# Copy the new directoryNames-Edgar.json file into the storage folder, replacing the old versions.
+#This ensures for 1.9 that the RIF-CS is generated using Groovy and not the old RIF-VM
+cp ../harvest/directoryNames-Edgar.json ../../storage/1793582ab247f6442162a75562dcc548/5d/23/cd/5d23cd8ff8e32c1b7859aa0fe4c40284/
+cp ../harvest/directoryNames-Edgar.json ../../storage/1793582ab247f6442162a75562dcc548/5d/1d/0a/5d1d0acd78059b9eac61bbfc728dcac9/
+
+# Copy the new enmass-dataset.json file into the storage folder, replacing the old versions.
+#This ensures for 1.9 that the RIF-CS is generated using Groovy and not the old RIF-VM
+cp ../harvest/enmasse-alerts/config/enmasse-dataset.json ../../storage/1793582ab247f6442162a75562dcc548/2f/b1/b5/2fb1b51a3c8dbbc4f35fa6328a8f2868/
+cp ../harvest/enmasse-alerts/config/enmasse-dataset.json ../../storage/1793582ab247f6442162a75562dcc548/d1/8e/c3/d18ec324ff4538e44d56409545ac5cf6/
